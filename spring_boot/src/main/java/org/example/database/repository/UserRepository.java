492dce0db78d5107e8a46500db2fe3c6c3f842bb
Konovalov-Konstantin <kon-kos87@yandex.ru>
2025-11-18 12:42:41 +0500

    запросы с сортировкой и пагинацией

--- spring_boot/src/main/java/org/example/database/repository/UserRepository.java
+++ spring_boot/src/main/java/org/example/database/repository/UserRepository.java
@@ -1,9 +1,16 @@
 package org.example.database.repository;
 
+import org.example.database.entity.Role;
 import org.example.database.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -18,4 +25,19 @@ public interface UserRepository extends JpaRepository<User, Long> {
     // пример с SQL
     @Query(value = "SELECT u.* FROM users u WHERE u.username = :username", nativeQuery = true)
     List<User> findAllByUserName(String username);
+
+    @Modifying(clearAutomatically = true)  // без этой аннотации при изменении данных в БД упадем с ошибкой
+                                           // clearAutomatically - удалет устаревшие данные из кэша после обновлении данных в БД
+    @Query("update User u set u.role = :role WHERE u.id in (:ids)")
+    int updateRole(Role role, Long... ids);
+
+    // динамическа сортировка (см. тест findTop3ByBirthDateBeforeTest)
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);   // вернет не более 3х User-ов
+
+    // запрос с пагинацией результата (см тест checkPagebleTest)
+    List<User> findAllBy(Pageable pageable);
+
+    // Page - подсписок списка объектов. Можно установить лимит на объектов на странице, номер страницы.
+    // Есть данные об общем кол-ве страниц (см тест findPageByTest)
+    Page<User> findPageBy(Pageable pageable);
 }
--- spring_boot/src/test/java/integration/repository/UserRepositoryTest.java
+++ spring_boot/src/test/java/integration/repository/UserRepositoryTest.java
@@ -1,19 +1,24 @@
 package integration.repository;
 
-import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.example.Application;
+import org.example.database.entity.Role;
 import org.example.database.entity.User;
 import org.example.database.repository.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestConstructor;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = Application.class)
 @ActiveProfiles("test")
@@ -34,5 +39,54 @@ class UserRepositoryTest {
     void findAllByUserNameTest() {
         List<User> allByUserName = userRepository.findAllByUserName("petr@gmail.com");
         assertThat(allByUserName).hasSize(1);
+    }
+
+    @Test
+    void updateRoleTest() {
+        User user = userRepository.getById(4L);
+        assertThat(user.getRole()).isEqualTo(Role.USER);
+
+        userRepository.updateRole(Role.ADMIN, 4L);
+        User updatedUser = userRepository.getById(4L);
+        assertThat(updatedUser.getRole()).isEqualTo(Role.ADMIN);
+    }
+
+    @Test
+    void findTop3ByBirthDateBeforeTest() {
+        // для динамической сортировки
+        // 1. вариант сортера с хардкодом
+        // Sort sortByLastNameAndFirstName = Sort.by("lastname").and(Sort.by("firstname"));
+        // 2. безопасный вариант сортера
+        Sort.TypedSort<User> sortBy = Sort.sort(User.class);
+        Sort sortByLastNameAndFirstName = sortBy.by(User::getFirstname).and(sortBy.by(User::getLastname));
+
+        List<User> top3ByBirthDateBeforeUsers = userRepository.findTop3ByBirthDateBefore(
+                LocalDate.now(), sortByLastNameAndFirstName);
+
+        assertThat(top3ByBirthDateBeforeUsers).hasSize(3);
+        assertEquals("Ivanov", top3ByBirthDateBeforeUsers.get(0).getLastname());
+    }
+
+    @Test
+    void checkPagebleTest() {
+        PageRequest pageable = PageRequest.of(1, 2, Sort.by("id"));
+        List<User> users = userRepository.findAllBy(pageable);
+        assertThat(users).hasSize(2);
+        assertEquals(3, users.get(0).getId());
+        assertEquals(4, users.get(1).getId());
+    }
+
+    @Test
+    void findPageByTest() {
+        PageRequest pageable = PageRequest.of(1, 2, Sort.by("id"));
+        Page<User> userPage = userRepository.findPageBy(pageable);
+        userPage.forEach(System.out::println);
+        assertThat(userPage).hasSize(2); // элементы 3 и4
+
+        while (userPage.hasNext()) {   // Page позволяет итерироваться по выборкам из БД
+            userPage = userRepository.findPageBy(userPage.nextPageable());
+            userPage.forEach(System.out::println);
+            assertThat(userPage).hasSize(1);   //  элемент 5
+        }
     }
 }
